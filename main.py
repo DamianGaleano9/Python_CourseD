6a91b18289849dae02f2948e4e3334bc7ed05969
Damian Galeano <dj.damian.b@hotmail.com>
2023-03-11 19:07:56 +0100

    Operators

--- main.py
+++ main.py
@@ -1,3 +1,38 @@
+#Python String Basics
+
+# """"
+# starter_sentence = "The quick brown fox jumped"
+# first_word = starter_sentence[0]
+# print(first_word)
+# sentence = "HELLO THERE!"
+# lowercase_sentence = sentence.lower()
+# print(lowercase_sentence)
+# """
+
+
+# #In the code below, assign the variable sub_sentence to be "pie" by selecting a range from "sentence".
+
+# """sentence = "Some pie please!"
+# sub_sentence = sentence[5:8]
+# print(sub_sentence)
+# """
+
+# """
+# Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo.
+# Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in.
+# Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
+# """
+
+# my_heredoc = """
+# Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo.
+# Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in.
+# Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
+# .strip()
+
+# """
+# print(my_heredoc)
+
+
 # """
 # [
 #     [0, 1, 2, 3, 4],
@@ -21,6 +56,97 @@
 #       counter += 1
 
 #   return matrix
-
-
 # print(manual_incrementing_matrix(9))
+
+
+
+
+# users = ["Damian", "Salvador", "Massimo"]
+# print(users)
+
+# users.append("Ana")
+# print(users)
+
+# tags = ["python", "development", "tutorial", "code"]
+
+
+# number_of_tags = len(tags)
+# last_item = tags[-1]
+# index_of_last_item = tags.index(last_item)
+# print(number_of_tags)
+# print(last_item)
+# print(index_of_last_item)
+
+
+
+# product_id = 123 #enter
+# float_number = 12.99 #float
+# fraction_number = 1/5
+# new_float_number  = float(product_id)
+# new_product_number = product_id + fraction_number
+
+# print(new_float_number)
+# print(type(product_id))
+# print(type(float_number))
+# print(type(fraction_number))
+# print(new_product_number)
+
+
+# print('Addition')
+# print(100 + 200)
+# print('---')
+
+# print('Subtraction')
+# print(200 -199)
+# print('---')
+
+# print('Division')
+# print(12.99/ 12.8888888)
+# print('---')
+
+# print('Floor Division') # Return an integer
+# print(12.99 // 12.8888888)
+# print('---')
+
+# print('Multiplication')
+# print(4.3 * 12.4)
+# print('---')
+
+# print('Modulus')
+# print(1 & 13)
+# print('---')
+
+# print('Exponential')
+# print(2 ** 2)
+
+# Please ---- Parentheses ()
+# Excuse  ------ Exponents ***
+# My ------ Multiplication *
+# Dear ---- Division /
+# Aaunt --- Addition +
+# Sally --- Subtraction -
+
+
+# 9 + 20 * 12 + (20 + 9) ** 2
+# 9 + 20 * 12 + 2900
+# 9 + 240 + 2900
+# 3149 
+
+# calculation = 9 + 20 * 12 + (20 + 9) ** 2
+
+# print(calculation)
+
+total = 100
+
+# total = total + 10
+
+# total += 10
+# total-= 10
+# total*= 10
+# total/= 10
+# total//= 10
+total **= 2
+total %= 10
+
+
+print(total)
